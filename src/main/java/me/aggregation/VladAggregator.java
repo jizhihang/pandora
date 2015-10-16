c137866bae8c56b3a495ce8731037712ba52d4e2
Akis Papadopoulos <iakopap@gmail.com>
2015-10-16 20:57:23 +0300

    Working on vlad aggregator in order to support multiple variant size vocabulary codebooks.

--- src/main/java/me/aggregation/VladAggregator.java
+++ src/main/java/me/aggregation/VladAggregator.java
@@ -5,14 +5,20 @@ import me.math.Normalizer;
 import me.math.PowerNormalizer;
 
 /**
- * An aggregator implementing the VLAD method to produce a fixed size normalized
+ * An aggregator implementing the vlad method to produce a fixed size normalized
  * (power, l2) vector given a variant number of local descriptors extracted from
- * a media item.
+ * a media item, supporting single or multiple vocabulary vectorization. In case
+ * of multiple vocabularies each sub bow vector generated independently from
+ * each vocabulary and then concatenated in a single vector.
  *
- * See more:<br/>
+ * See more for vlad method:<br/>
  * <em>H. Jegou, F. Perronnin, M. Douze, J. Sanchez, P. Perez, and C. Schmid,
  * Aggregating local image descriptors into compact codes, IEEE Transactions on
  * Pattern Analysis and Machine Intelligence, 2012.</em>
+ *
+ * See more for multiple vocabularies:
+ * <em>Jégou, H., & Chum, O. (2012). Negative evidences and co-occurences in
+ * image retrieval: The benefit of PCA and whitening. In ECCV 2012.</em>
  *
  * This class is a modification of a class written by Elefterios
  * Spyromitros-Xioufis, please see <a href="https://goo.gl/xiSHRJ">more</a>.
@@ -21,8 +27,8 @@ import me.math.PowerNormalizer;
  */
 public class VladAggregator implements Aggregator {
 
-    // Vocabulary codebook
-    private Codebook codebook;
+    // Vocabulary codebooks
+    private Codebook[] codebooks;
 
     // Normalization
     private boolean normalize = true;
@@ -34,14 +40,14 @@ public class VladAggregator implements Aggregator {
     Normalizer euclidean;
 
     /**
-     * A constructor initiating the vocabulary codebook of centroid words plus
+     * A constructor initiating the vocabulary codebooks of centroid words plus
      * the normalization option.
      *
-     * @param codebook the vocabulary codebook.
+     * @param codebooks the vocabulary codebooks.
      * @param normalize the normalization option.
      */
-    public VladAggregator(Codebook codebook, boolean normalize) {
-        this.codebook = codebook;
+    public VladAggregator(Codebook[] codebooks, boolean normalize) {
+        this.codebooks = codebooks;
 
         this.normalize = normalize;
 
@@ -58,27 +64,49 @@ public class VladAggregator implements Aggregator {
      */
     @Override
     public double[] aggregate(double[][] descriptors) {
-        // Calculating the size of the vlad vector
-        int size = codebook.getSize() * codebook.getWidth();
+        // Calculating the final vector size
+        int size = 0;
+
+        // Regarding the number of codebooks their size and width
+        for (Codebook codebook : codebooks) {
+            size += codebook.getSize() * codebook.getWidth();
+        }
 
         double[] vlad = new double[size];
 
-        // Accumulating the residues per descriptor
-        for (double[] descriptor : descriptors) {
-            // Finding the nearest centroid index of the descriptor
-            int index = codebook.getNearestCentroidIndex(descriptor);
+        int offset = 0;
 
-            // Accumulating the residues from the nearest centroid
-            for (int i = 0; i < descriptor.length; i++) {
-                double component = codebook.getComponent(index, i);
+        // Regarding each codebook
+        for (Codebook codebook : codebooks) {
+            // Building the vlad subvector for the next codebook
+            double[] subvector = new double[codebook.getSize() * codebook.getWidth()];
 
-                vlad[index * descriptor.length + i] += descriptor[i] - component;
+            // Accumulating the residues per descriptor
+            for (double[] descriptor : descriptors) {
+                // Finding the nearest centroid index of the descriptor
+                int index = codebook.getNearestCentroidIndex(descriptor);
+
+                // Accumulating the residues from the nearest centroid
+                for (int i = 0; i < descriptor.length; i++) {
+                    double component = codebook.getComponent(index, i);
+
+                    subvector[index * descriptor.length + i] += descriptor[i] - component;
+                }
             }
+
+            // Normalize using Power and Euclidean l2 norms
+            if (normalize) {
+                power.normalize(vlad);
+                euclidean.normalize(vlad);
+            }
+
+            // Concatenate the subvector
+            System.arraycopy(subvector, 0, vlad, offset, subvector.length);
+            offset += subvector.length;
         }
 
-        // Normalize using Power and Euclidean l2 norms
-        if (normalize) {
-            power.normalize(vlad);
+        // Normalizing final vector only in case of multiple vocabularies
+        if (codebooks.length > 1 && normalize) {
             euclidean.normalize(vlad);
         }
 
