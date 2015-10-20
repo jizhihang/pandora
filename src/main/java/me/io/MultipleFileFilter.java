62eac7b159a170cc6a380f9912dc0b5b40b28b54
Akis Papadopoulos <iakopap@gmail.com>
2015-10-20 10:49:35 +0300

    Adding a custom multiple file filter.

---
+++ src/main/java/me/io/MultipleFileFilter.java
@@ -0,0 +1,43 @@
+package me.io;
+
+import java.io.File;
+import java.io.FileFilter;
+
+/**
+ * A multiple custom file filter, supporting user defined file types.
+ *
+ * @author Akis Papadopoulos
+ */
+public class MultipleFileFilter implements FileFilter {
+
+    // Supported file types
+    private String[] patterns;
+
+    /**
+     * A constructor initiating user defined multiple patterns.
+     *
+     * @param patterns multiple user define patterns.
+     */
+    public MultipleFileFilter(String... patterns) {
+        this.patterns = patterns;
+    }
+
+    /**
+     * A method filtering the file regarding the extension.
+     *
+     * @param file the file to be filtered.
+     * @return true if extension matching at least one pattern, otherwise false.
+     */
+    @Override
+    public boolean accept(File file) {
+        for (String pattern : patterns) {
+            String name = file.getName().toLowerCase();
+
+            if (name.endsWith("." + pattern.toLowerCase())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
