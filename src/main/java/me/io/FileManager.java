0fc14d61e31f105dadfbeeee044617ef1292e08f
Akis Papadopoulos <iakopap@gmail.com>
2015-10-20 20:18:51 +0300

    Adding file name filter for images and a write method in file manager plus minor documentation changes.

--- src/main/java/me/io/FileManager.java
+++ src/main/java/me/io/FileManager.java
@@ -9,12 +9,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A singleton file system manager.
+ * A singleton system file manager.
  *
  * @author Akis Papadopoulos
  */
 public final class FileManager {
 
+    /**
+     * A method reading a vector written in comma separated form in a binary
+     * file.
+     *
+     * @param filepath the absolute path to the file.
+     * @return the vector.
+     * @throws IOException an unknown exception.
+     */
     public static double[] readVector(String filepath) throws IOException {
         BufferedReader reader = null;
 
@@ -44,6 +52,13 @@ public final class FileManager {
         }
     }
 
+    /**
+     * A method reading a matrix written in line-by-line form to a binary file.
+     *
+     * @param filepath the absolute path to the file.
+     * @return the matrix.
+     * @throws IOException an unknown exception.
+     */
     public static double[][] readMatrix(String filepath) throws IOException {
         BufferedReader reader = null;
 
@@ -87,19 +102,71 @@ public final class FileManager {
         }
     }
 
+    /**
+     * A method writing a given vector to a binary file in comma separated form.
+     *
+     * @param vector the vector.
+     * @param filepath the absolute path to the file.
+     * @throws IOException an unknown exception.
+     */
     public static void writeVector(double[] vector, String filepath) throws IOException {
-        System.out.println(filepath);
-
         BufferedWriter writer = null;
 
         try {
+            // Opening a write output stream
             writer = new BufferedWriter(new FileWriter(filepath));
 
+            // Writing components in comma separated form
             for (int j = 0; j < vector.length; j++) {
                 writer.write(String.valueOf(vector[j]));
 
                 if (j < vector.length - 1) {
                     writer.write(",");
+                }
+            }
+        } catch (IOException exc) {
+            throw exc;
+        } finally {
+            if (writer != null) {
+                writer.flush();
+                writer.close();
+            }
+        }
+    }
+
+    /**
+     * A method writing a given matrix in a binary file in line-by-line form.
+     *
+     * @param matrix the matrix.
+     * @param filename the absolute path to the file.
+     * @throws IOException an unknown exception.
+     */
+    public static void writeMatrix(double[][] matrix, String filename) throws IOException {
+        BufferedWriter writer = null;
+
+        try {
+            // Opening a file output stream
+            writer = new BufferedWriter(new FileWriter(filename));
+
+            // Writing line-byline each row
+            for (int i = 0; i < matrix.length; i++) {
+                double[] row = matrix[i];
+
+                // Building each row in a comma separated line
+                StringBuilder line = new StringBuilder();
+
+                for (int j = 0; j < row.length; j++) {
+                    line.append(row[j]);
+
+                    if (j < row.length - 1) {
+                        line.append(",");
+                    }
+                }
+
+                writer.write(line.toString());
+
+                if (i < matrix.length - 1) {
+                    writer.newLine();
                 }
             }
         } catch (IOException exc) {
