aff03b67667c13f9478673b3575b1467a764d0b0
Akis Papadopoulos <iakopap@gmail.com>
2015-10-26 21:48:31 +0200

    Extending build in properties to support matching multiple properties identified by given regex pattern.

---
+++ src/main/java/me/util/SmartProperties.java
@@ -0,0 +1,46 @@
+package me.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * A extended version of the build-in properties class.
+ *
+ * @author Akis Papadopoulos
+ */
+public class SmartProperties extends Properties {
+
+    /**
+     * A method returning matching property values given a regex pattern.
+     *
+     * @param pattern the regex pattern to match.
+     * @return a list of values.
+     */
+    public List<String> matchProperties(String pattern) {
+        List<String> matched = new ArrayList<String>();
+
+        for (Enumeration keys = this.keys(); keys.hasMoreElements();) {
+            String key = (String) keys.nextElement();
+
+            if (key.matches(pattern)) {
+                matched.add(key);
+            }
+        }
+
+        // Sorting alphabetically because order is important
+        Collections.sort(matched);
+
+        List<String> values = new ArrayList<String>();
+
+        for (String key : matched) {
+            String value = this.getProperty(key);
+
+            values.add(value);
+        }
+
+        return values;
+    }
+}
