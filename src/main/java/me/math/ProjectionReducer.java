61162f3e522d52feacd8a2647e1fa8d53d16ae66
Akis Papadopoulos <iakopap@gmail.com>
2015-10-30 16:28:26 +0200

    Updating various documentations in projection space implementation.

--- src/main/java/me/math/ProjectionReducer.java
+++ src/main/java/me/math/ProjectionReducer.java
@@ -6,8 +6,8 @@ import org.ejml.data.DenseMatrix64F;
 import org.ejml.ops.CommonOps;
 
 /**
- * A component reducer given the projection principal component eigen values
- * sub-space stripped off to the most dominant components.
+ * A component reducer given the projection principal component sub-space
+ * stripped off to the most dominant components.
  *
  * This class is a modification of a class written by Peter Abeles, please see
  * <a href="https://goo.gl/sdBCiC">more</a>.
@@ -16,7 +16,7 @@ import org.ejml.ops.CommonOps;
  */
 public class ProjectionReducer implements ComponentReducer {
 
-    // Most dominant principal component eigen value sub-space
+    // Most dominant principal component eigenvectors
     private DenseMatrix64F B_t;
 
     // Adjustment mean component vector
@@ -24,10 +24,9 @@ public class ProjectionReducer implements ComponentReducer {
 
     /**
      * A constructor creating a projection reducer given the principal component
-     * eigen values sub-space to the most dominant components and the adjustment
-     * mean vector.
+     * sub-space to the most dominant components and the adjustment mean vector.
      *
-     * @param subspace the principal component eigen values sub-space.
+     * @param subspace the principal component sub-space.
      * @param mean the adjustment mean vector.
      */
     public ProjectionReducer(double[][] subspace, double[] mean) {
@@ -37,9 +36,10 @@ public class ProjectionReducer implements ComponentReducer {
     }
 
     /**
-     * A constructor creating a projection reducer given the line based file
-     * containing both the adjustment mean vector followed by the eigen values
-     * sub-space.
+     * A constructor creating a projection reducer given the file containing in
+     * the first line the adjustment mean vector followed in the subsequent
+     * lines by the most dominant principal eigenvectors in descending
+     * eigenvalues order.
      *
      * @param filepath the absolute path to the projection sub-space file.
      * @throws IOException unknown IO exceptions.
@@ -47,12 +47,12 @@ public class ProjectionReducer implements ComponentReducer {
     public ProjectionReducer(String filepath) throws IOException {
         double[][] lines = Reader.read(filepath);
 
-        // Loading 1st line as the mean adjustment vector
+        // Loading the mean adjustment vector
         double[] mean = lines[0];
 
         m = DenseMatrix64F.wrap(mean.length, 1, mean);
 
-        // Loading the eigen values starting from the 2nd line
+        // Loading the most dominant eigenvectors
         B_t = new DenseMatrix64F(lines.length - 1, lines[1].length);
 
         for (int i = 1; i < lines.length; i++) {
--- src/main/java/me/math/ProjectionSpace.java
+++ src/main/java/me/math/ProjectionSpace.java
@@ -10,7 +10,7 @@ import org.ejml.ops.SingularOps;
 /**
  * A projection space upon a vector data set using principal component analysis
  * based on singular value decomposition, where most dominant principal
- * components are ordered in descending order.
+ * components are ordered in descending eigenvalues order.
  *
  * This class is a modification of a class written by Peter Abeles, please see
  * <a href="https://goo.gl/sdBCiC">more</a>.
@@ -19,17 +19,18 @@ import org.ejml.ops.SingularOps;
  */
 public class ProjectionSpace {
 
-    // Principal component eigen value space
+    // Principal component eigenvectors
     private DenseMatrix64F V_t;
 
     // Adjustment mean vector
     private DenseMatrix64F m;
 
     /**
-     * A constructor creating a projection space of eigen values given the
-     * original data. Eigen values are ordered descending indicating the most
-     * dominant principal components come first. Using compact form means the
-     * eigen values will be stripped off equal to the number of the data items.
+     * A constructor creating a projection space given the original data, where
+     * eigenvectors are stored in descending eigenvalues order. Order indicates
+     * that the most dominant principal components come first. Using compact
+     * form means the projection space will be stripped off to the first in
+     * order eigenvectors, equal to the number of the original data items.
      *
      * @param data the original data.
      * @param compact true to create space in compact form.
@@ -65,22 +66,23 @@ public class ProjectionSpace {
             throw new RuntimeException("Singular value decomposition process failed");
         }
 
-        // Computing the principal component eigen value space
+        // Computing the principal component eigenvectors transposed matrix
         V_t = svd.getV(null, true);
 
+        // Compunting the eigenvalues corresponding to the eigenvectors
         DenseMatrix64F W = svd.getW(null);
 
-        // Ordering singular eigen values desceding
+        // Ordering singular eigenvalues and vectors in desceding order
         SingularOps.descendingOrder(null, false, W, V_t, true);
 
         m = DenseMatrix64F.wrap(mean.length, 1, mean);
     }
 
     /**
-     * A constructor creating a projection space given the eigen values and the
-     * adjustment mean vector.
+     * A constructor creating a projection space given the eigenvectors in
+     * descending eigenvalues order and the adjustment mean vector.
      *
-     * @param space the projection eigen value space.
+     * @param space the eigenvectors matrix.
      * @param mean the adjustment mean vector.
      */
     public ProjectionSpace(double[][] space, double[] mean) {
@@ -90,8 +92,9 @@ public class ProjectionSpace {
     }
 
     /**
-     * A constructor creating a projection space given the line based file
-     * containing both the adjustment mean vector followed by the eigen values.
+     * A constructor creating a projection space given the file containing in
+     * the first line the adjustment mean vector followed in the subsequent
+     * lines by the eigenvectors in descending eigenvalues order.
      *
      * @param filepath the absolute path to the projection space file.
      * @throws IOException unknown IO exceptions.
@@ -99,12 +102,12 @@ public class ProjectionSpace {
     public ProjectionSpace(String filepath) throws IOException {
         double[][] lines = Reader.read(filepath);
 
-        // Loading 1st line as the mean adjustment vector
+        // Loading the mean adjustment vector
         double[] mean = lines[0];
 
         m = DenseMatrix64F.wrap(mean.length, 1, mean);
 
-        // Loading the eigen values starting from the 2nd line
+        // Loading the eigenvectors matrix
         V_t = new DenseMatrix64F(lines.length - 1, lines[1].length);
 
         for (int i = 1; i < lines.length; i++) {
@@ -115,9 +118,10 @@ public class ProjectionSpace {
     }
 
     /**
-     * A method returning the projection space of eigen values.
+     * A method returning the projection space constituted by the eigenvectors
+     * in descending eigenvalues order.
      *
-     * @return the projection space of eigen values.
+     * @return the projection eigenvectors matrix.
      */
     public double[][] getSpace() {
         double[][] space = new double[V_t.numRows][V_t.numCols];
@@ -133,10 +137,12 @@ public class ProjectionSpace {
 
     /**
      * A method returning a sub-space of the projection space given the number
-     * of the most dominant principal components to retain.
+     * of the most dominant principal components to retain. The eigenvectors
+     * matrix will be stripped off to the first in order eigenvectors evaluated
+     * by the eigenvalues in descending order.
      *
      * @param size the number of the most dominant components to retain.
-     * @return a projection sub-space to the most dominant components.
+     * @return the eigenvectors matrix to the most dominant components.
      */
     public double[][] getBasis(int size) {
         // TODO: Implement a more memory wised matrix copy
